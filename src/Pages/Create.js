9b6d397a91ab62433737dfaf0ec1595df3c31ee4
brayanmcz <brayan.mcz@gmail.com>
2019-04-02 04:44:25 -0700

    Routing for pages

---
+++ src/Pages/Create.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CreatePage extends Component {
+    render(){
+        return(
+            <p>Create Page</p>
+        )
+    }
+}
+
+export default CreatePage;
---
+++ src/Pages/Lobby.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LobbyPage extends Component {
+    render(){
+        return(
+            <p>Lobby Page</p>
+        )
+    }
+}
+
+export default LobbyPage;
